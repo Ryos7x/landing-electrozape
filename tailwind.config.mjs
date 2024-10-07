69421caedd087641d6386d3ee616b681456ba738
Ryos7x <eliastello.dev@gmail.com>
2024-10-07 15:24:48 -0300

    Added home, problem & contact sections

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,19 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+      colors: {
+        primaryRed: "#FF0000",
+        secondaryRed: "#CC0000",
+        textColor: "#5F2727",
+        darkBackground: "#111111",
+        whiteBackground: "#FCFCFD",
+        redBackground: "#530000",
+      },
+    },
   },
   plugins: [],
 };
